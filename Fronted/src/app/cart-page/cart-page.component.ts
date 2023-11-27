376742537eeab875afa0606877ef52cde8ce81bc
12CUY <wiliampuntomorales45@gmail.com>
2023-11-26 20:50:03 -0500

    usa de la libreria sweetalert en envio

--- Fronted/src/app/cart-page/cart-page.component.ts
+++ Fronted/src/app/cart-page/cart-page.component.ts
@@ -3,51 +3,67 @@ import { cart, priceSummary } from '../data-type';
 import { ProductService } from '../services/product.service';
 import { Router } from '@angular/router';
 
-
+// Mi carrito
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
   styleUrls: ['./cart-page.component.css']
 })
 export class CartPageComponent implements OnInit {
-  cartData:cart[]|undefined;
+  // Arreglo 
+  // que almacena los detalles de los productos en el carrito
+  cartData:cart[] | undefined;
+
+  //objeto
+  // contiene el resumen de precios 
   priceSummary:priceSummary={
-   price:0,
-   discount:0,
-   iva:0,
-   delivery:0,
-   total:0
+    price: 0,
+    iva: 0,
+    delivery: 0,
+    total: 0,
+    discount: 0
   }
+
   constructor(private product:ProductService, private router:Router){}
+
   ngOnInit(): void {
+    //cargar los detalles del carrito al iniciar el componente
     this.loadDetails();
   }
-  removeToCart(cartId:number|undefined){
-    cartId && this.cartData && this.product.removeToCart(cartId)
-    .subscribe(()=>{
-      this.loadDetails();
-    });
+  // Método para eliminar un producto del carrito
+  removeToCart(cartId: number | undefined): void {
+    cartId &&
+      this.cartData &&
+      this.product.removeToCart(cartId).subscribe(() => {
+        // Recargar los detalles después de la eliminación
+        this.loadDetails();
+      });
   }
-  loadDetails(){
-    this.product.currentCart().subscribe((result)=>{
-      this.cartData=result;
-      let price=0;
-      result.forEach((item)=>{
-        if(item.quantity){
-         price=price+ (+item.price* + item.quantity)
+
+  // Método para cargar los detalles del carrito y calcular el resumen de precios
+  loadDetails(): void {
+    this.product.currentCart().subscribe((result) => {
+      this.cartData = result;
+      let price = 0;
+
+      result.forEach((item) => {
+        if (item.quantity) {
+          price = price + +item.price * +item.quantity;
         }
       });
-      this.priceSummary.price=price;
-      this.priceSummary.discount=price/50;
-      this.priceSummary.iva=price*0.5;
-      this.priceSummary.delivery=7;
-      this.priceSummary.total=price+(price*0.12)+7-(price/50);
-      if(!this.cartData.length){
+      // metodos de calculo del total de productos
+      this.priceSummary.price = +price.toFixed(2);
+      this.priceSummary.iva = +(price * 0.12).toFixed(2);
+      this.priceSummary.delivery = 7;
+      this.priceSummary.total = +(price + this.priceSummary.iva + this.priceSummary.delivery).toFixed(2);
+
+      if (!this.cartData.length) {
         this.router.navigate(['/']);
       }
-     });
+    });
   }
-  checkout(){
+  checkout():void{
+    // navega a la pagina de pago al finalizar la compra 
     this.router.navigate(['/checkout']);
   }
 }
--- Fronted/src/app/checkout/checkout.component.ts
+++ Fronted/src/app/checkout/checkout.component.ts
@@ -1,9 +1,8 @@
-import { cart, order } from '../data-type';
-import { Component} from '@angular/core';
+import { cart, order} from '../data-type';
+import { Component } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Router } from '@angular/router';
-
-
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-checkout',
@@ -11,51 +10,67 @@ import { Router } from '@angular/router';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent {
-  totalPrice:number | undefined;
-  cartData:cart[]|undefined;
-  orderMsg:string|undefined;
-  constructor(private product:ProductService,private router: Router){}
-  ngOnInit():void{
-    this.product.currentCart().subscribe((result)=>{
-      let price=0;
-      this.cartData=result;
-      result.forEach((item)=>{
-        if(item.quantity){
-         price=price+ (+item.price* + item.quantity);
+  totalPrice: number | undefined; // Variable para almacenar el precio total de la orden
+  cartData: cart[] | undefined; // Datos del carrito
+  orderMsg: string | undefined; // Mensaje de estado de la orden
+
+  constructor(private product: ProductService, private router: Router) {}
+
+  ngOnInit(): void {
+    // Al inicializar el componente, obtenemos los detalles del carrito y calculamos el precio total
+    this.product.currentCart().subscribe((result) => {
+      let price = 0;
+      this.cartData = result;
+
+      // Calcula el precio total sumando los productos en el carrito
+      result.forEach((item) => {
+        if (item.quantity) {
+          price = price + (+item.price * +item.quantity);
         }
       });
-      this.totalPrice=price+(price*0.12)+7-(price/50);;
-      console.warn(this.totalPrice);
-     });
 
-  }
-  orderNow(data:{email:string,address:string,contact:string}){
-   let user = localStorage.getItem('user');
-   let userId= user && JSON.parse(user).id;
-
-   if(this.totalPrice){
-    let orderData:order={
-      ...data,
-      totalPrice:this.totalPrice,
-      userId,
-      id:undefined
-    }
-    this.cartData?.forEach((item)=>{
-     setTimeout(()=>{
-      item.id && this.product.deleteCartItems(item.id);
-     },700);
+      // Calcula el precio total con impuestos, envío y descuento
+      this.totalPrice = +(price + (price * 0.12) + 7).toFixed(2);
+      console.warn(this.totalPrice); // Muestra el precio total en la consola
     });
+  }
 
-    this.product.orderNow(orderData).subscribe((result)=>{
-      if(result){
-        this.orderMsg="Orden realizada con exito";
-         setTimeout(() => {
-          this.router.navigate(['/mis-ordenes']);
-          this.orderMsg=undefined;
-         }, 4000);
-      }
-    })
-   }
+  orderNow(data: { email: string; address: string; contact: string }) {
+    let user = localStorage.getItem('user');
+    let userId = user && JSON.parse(user).id;
+
+    if (this.totalPrice) {
+      // Crea un objeto de orden con la información del usuario y el precio total
+      let orderData: order = {
+        ...data,
+        totalPrice: this.totalPrice,
+        userId,
+        id: undefined
+      };
+
+      // Elimina los elementos del carrito uno por uno después de un breve retraso
+      this.cartData?.forEach((item) => {
+        setTimeout(() => {
+          item.id && this.product.deleteCartItems(item.id);
+        }, 700);
+      });
+
+      this.product.orderNow(orderData).subscribe((result) => {
+        if (result) {
+          // Utiliza SweetAlert para mostrar un mensaje de éxito
+          Swal.fire({
+            icon: 'success',
+            title: '¡Orden realizada con éxito!',
+            text: 'Serás redirigido a tus órdenes en unos segundos...',
+            timer: 4000, // 4 segundos
+            timerProgressBar: true,
+            showConfirmButton: false
+          }).then(() => {
+            // Redirige a la página de órdenes después de 4 segundos
+            this.router.navigate(['/mis-ordenes']);
+          });
+        }
+      });
+    }
   }
 }
-
