3a983fb5d9ec6921e25a9cc37dc8ccd52d394b57
12CUY <wiliampuntomorales45@gmail.com>
2023-12-09 18:20:19 -0500

    actualizar

--- Fronted/src/app/vendedor-update-product/vendedor-update-product.component.ts
+++ Fronted/src/app/vendedor-update-product/vendedor-update-product.component.ts
@@ -1,53 +1,61 @@
-import { Component,OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { product } from '../data-type';
 import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-vendedor-update-product',
   templateUrl: './vendedor-update-product.component.html',
-  styleUrls: ['./vendedor-update-product.component.css']
+  styleUrls: ['./vendedor-update-product.component.css'],
 })
-
-
 export class VendedorUpdateProductComponent implements OnInit {
-  productData:undefined | product;
-  productMessage:undefined | string;
-  constructor (private route: ActivatedRoute, private product:ProductService) {}
+  productData: undefined | product;
+  productMessage: undefined | string;
 
+  constructor(
+    private route: ActivatedRoute,
+    private product: ProductService,
+    private router: Router
+  ) {}
 
-//obtener información sincrónica(snapshot)
-//mapa de los parámetros (paramMap)
-
-ngOnInit(): void {
-  let productId= this.route.snapshot.paramMap.get('id');
+  ngOnInit(): void {
+    let productId = this.route.snapshot.paramMap.get('id');
     console.warn(productId);
-    productId && this.product.getProduct(productId).subscribe((data)=>{
-      console.warn(data);
-      this.productData=data;
-    });
+    productId &&
+      this.product.getProduct(productId).subscribe((data) => {
+        console.warn(data);
+        this.productData = data;
+      });
   }
 
-//aqui se asigna un id al data (prodcuto)
-
-  submit(data:product){
+  submit(data: product) {
     console.warn(data);
-    if(this.productData){
-     data.id=this.productData.id;
+    if (this.productData) {
+      data.id = this.productData.id;
     }
 
-  //actualizar la información del producto (updateProduct)  
+    // Actualizar la información del producto (updateProduct)
+    this.product.updateProduct(data).subscribe((result) => {
+      if (result) {
+        this.productMessage = 'El producto ha sido actualizado';
 
-    this.product.updateProduct(data).subscribe((result)=>{
-      if(result){
-        this.productMessage="El producto a sido actualizado";
+        // Muestra SweetAlert con mensaje
+        Swal.fire({
+          icon: 'success',
+          title: 'Éxito',
+          text: 'El producto ha sido actualizado',
+          confirmButtonText: 'OK',
+        }).then(() => {
+          // Redirige a vendedor-home después de hacer clic en OK
+          this.router.navigate(['/vendedor-home']);
+        });
       }
     });
 
-  //se establece un temporizador 
-   
-    setTimeout(()=>{
-      this.productMessage=undefined;
-    },3000);
+    // Establece un temporizador para limpiar el mensaje
+    setTimeout(() => {
+      this.productMessage = undefined;
+    }, 3000);
   }
 }
