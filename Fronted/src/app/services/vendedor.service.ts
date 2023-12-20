4e8389f06b754982e056c2159ac8e4f7ca093ea0
12CUY <wiliampuntomorales45@gmail.com>
2023-12-20 00:31:29 -0500

    cambios vendedor

--- Fronted/src/app/services/vendedor.service.ts
+++ Fronted/src/app/services/vendedor.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { login, SignUp } from '../data-type';
 import { BehaviorSubject } from 'rxjs';
 import { Router } from '@angular/router';
+import * as bcrypt from 'bcryptjs'; // Importa el paquete bcryptjs
 
 @Injectable({
   providedIn: 'root'
@@ -14,20 +15,29 @@ export class VendedorService {
 
   constructor(private http: HttpClient, private router: Router) {}
 
-  userSignUp(data: SignUp) {
-    this.http.post(`${this.baseUrl}/vendedor`, data, { observe: 'response' }).subscribe(
-      (response: any) => {
-        // Manejar la respuesta del servidor
-        console.log('Registro exitoso:', response);
-        this.isSellerLoggedIn.next(true);
-        localStorage.setItem('vendedor', JSON.stringify(response.body));
-        this.router.navigate(['vendedor-home']);
-      },
-      (error) => {
-        // Manejar el error
-        console.error('Error al registrar vendedor:', error);
-      }
-    );
+  async userSignUp(data: SignUp) {
+    try {
+      // Hashea la contraseña antes de enviarla al servidor
+      const hashedPassword = await bcrypt.hash(data.password, 10);
+
+      const requestData = { ...data, password: hashedPassword };
+
+      this.http.post(`${this.baseUrl}/vendedor`, requestData, { observe: 'response' }).subscribe(
+        (response: any) => {
+          // Manejar la respuesta del servidor
+          console.log('Registro exitoso:', response);
+          this.isSellerLoggedIn.next(true);
+          localStorage.setItem('vendedor', JSON.stringify(response.body));
+          this.router.navigate(['vendedor-home']);
+        },
+        (error) => {
+          // Manejar el error
+          console.error('Error al registrar vendedor:', error);
+        }
+      );
+    } catch (error) {
+      console.error('Error al hashear la contraseña:', error);
+    }
   }
 
   reloadVendedor() {
@@ -37,19 +47,28 @@ export class VendedorService {
     }
   }
 
-  userLogin(data: login) {
-    this.http.post(`${this.baseUrl}/vendedor/login`, data, { observe: 'response' }).subscribe(
-      (response: any) => {
+  async userLogin(data: login) {
+    try {
+      const response: any = await this.http.post(`${this.baseUrl}/vendedor/login`, data, { observe: 'response' }).toPromise();
+      
+      const user = response.body;
+  
+      // Compara la contraseña proporcionada con la contraseña hasheada almacenada en el servidor
+      const isPasswordValid = await bcrypt.compare(data.password, user.password);
+  
+      if (isPasswordValid) {
         // Manejar la respuesta del servidor
-        console.log('Usuario Logueado:', response);
-        localStorage.setItem('vendedor', JSON.stringify(response.body));
+        console.log('Usuario Logueado:', user);
+        localStorage.setItem('vendedor', JSON.stringify(user));
         this.router.navigate(['vendedor-home']);
-      },
-      (error) => {
-        // Manejar el error
-        console.error('No se encuentra logueado:', error);
+      } else {
+        console.error('Credenciales inválidas');
         this.isLoginError.emit(true);
       }
-    );
+    } catch (error) {
+      // Manejar el error
+      console.error('No se encuentra logueado:', error);
+      this.isLoginError.emit(true);
+    }
   }
 }
